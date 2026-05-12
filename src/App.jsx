628a5d4329f47b55f0d917772eab4e0fdaa56318
LUCA GAMAS DE MAIO <49008593@ort>
2026-05-12 14:10:12 -0300

    done api connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
-
+import {get10pics}from "./api_connection/api.js"
 import './App.css'
 
 function App() {
+  const [response, setResponse] = useState([])
   
+
+  const trye = async () =>{
+    const e = await get10pics()
+    setResponse( e)
+    console.log(response)
+  }
 
   return (
     <>
+        <button onClick={()=>trye()}>eeeeeee</button>
+        {response[0] && (<img src={response[0].url} alt="eee" />)}
         <p>hola soy homero chino</p>
     </>
   )
 }
 
 export default App
+
+

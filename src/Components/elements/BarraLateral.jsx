fc1aa40368f0b0e1f04a1207371e0dae9c255ab7
Luca Gamas <lucagamasdemaio@gmail.com>
2026-05-19 15:02:42 -0300

    minor changes (routes)

--- src/Components/elements/BarraLateral.jsx
+++ src/Components/elements/BarraLateral.jsx
@@ -1,21 +1,24 @@
 import { useState } from 'react'
 import "./BarraLateral.css"
+import { useNavigate } from "react-router-dom";
 
 function BarraLateral() {
+    ///miperfil
+    const navigate = useNavigate();
 
     return (
 
         <section>
             <button><img src="" alt="" /></button>
             <ul>
-                <li><img src="" alt="" /><button>Home</button></li>
+                <li><img src="" alt="" /><button onClick={() => navigate("/")}>Home</button></li>
                 <li><img src="" alt="" /><button>Search</button></li>
                 <li><img src="" alt="" /><button>Explore</button></li>
                 <li><img src="" alt="" /><button>Reels</button></li>
                 <li><img src="" alt="" /><button>Messages</button></li>
                 <li><img src="" alt="" /><button>Notifications</button></li>
                 <li><img src="" alt="" /><button>Create</button></li>
-                <li><img src="" alt="" /><button>Profile</button></li>
+                <li><img src="" alt="" /><button onClick={() => navigate("/miperfil")}>Profile</button></li> 
                 <li><img src="" alt="" /><button>More</button></li>
             </ul>
         </section>

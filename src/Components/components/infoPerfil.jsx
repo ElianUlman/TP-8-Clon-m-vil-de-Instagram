09cb94b4f3ed1e273440a556747b422dbc089dd2
Luca Gamas <lucagamasdemaio@gmail.com>
2026-05-26 15:10:38 -0300

    profile now renders myProfile

--- src/Components/components/infoPerfil.jsx
+++ src/Components/components/infoPerfil.jsx
@@ -3,25 +3,28 @@ import "./infoPerfil.css"
 import  FollowBtn  from "../elements/followBtn.jsx"
 import  BarraEstados  from "./BarraEstados.jsx" 
 function InfoPerfil({ perfil }) {
-
+    let miPerfil = JSON.parse(localStorage.getItem("myprofile")) || ""
+    
     return (
 
         <div>
 
-            <img src={perfil.url} alt="" />
+            <img src={perfil.url} alt="image nou work :c" />
             <h5>{perfil.nombre || "nombre perfil"}</h5>
-            <FollowBtn id = {perfil.id}/>
+            
+            {/**el codigo que justo aca abajo hice solo para follow lo pueden poner para el resto de cosas */}
+            {miPerfil?.id != perfil.id && <FollowBtn id = {perfil.id}/>}
             <button>Message</button>
-            <button><img src="" alt="" /></button>
+            <button><img src="#" alt="aniadir" /></button>
             <button>...</button>
 
             <div>
                 <p>{perfil.posts.length} Posts</p>
-                <p>{perfil.followers.length} Follower</p>
-                <p>{perfil.following.length} Following</p>
+                <p>{perfil.followers?.length || 67} Follower</p>
+                <p>{perfil.following?.length || 76} Following</p>
             </div>
 
-            <p>{perfil.description}</p>
+            <p>{perfil.description || "descripcion perfil"}</p>
 
             <BarraEstados estados={perfil.estadosGuardados}/>
             
--- src/Components/pages/Profile.jsx
+++ src/Components/pages/Profile.jsx
@@ -1,12 +1,75 @@
-import { use, useState } from 'react'
+import { use, useState, useEffect } from 'react'
+import { getXpics, getCatById } from '../../api_connection/api'
 import InfoPerfil from "../components/infoPerfil.jsx"
+import PublicacionesPerfil from '../components/PublicacionesPerfil.jsx'
 
-function Profile( ){
+function Profile() {
+
+    
+
+    const [perfil, setPerfil] = useState()
+    const [misPosts, setMisPosts] = useState()
+    const [misEstados, setMisEstados] = useState()
+
+    const [miPerfil, setMiPerfil] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+    
+
+    useEffect(() => {
 
 
-    return(
+        async function loadData() {
+
+            if (miPerfil == null) {
+
+                let storedPerfil = JSON.parse(localStorage.getItem("myprofile")) || ""
+                if (storedPerfil == "") {
+                    storedPerfil = (await getXpics(1))[0]
+                    localStorage.setItem("myprofile", JSON.stringify(storedPerfil))
+                    setPerfil(storedPerfil)
+                } else {
+                    setPerfil(storedPerfil)
+                }
+
+                let storedMyPosts = JSON.parse(localStorage.getItem("myposts")) || ""
+                if (storedMyPosts == "") {
+                    storedMyPosts = await getXpics(10)
+                    localStorage.setItem("myposts", JSON.stringify(storedMyPosts))
+                    setMisPosts(storedMyPosts)
+                } else {
+                    setMisPosts(storedMyPosts)
+                }
+
+                let storedMisEstados = JSON.parse(localStorage.getItem("mystories")) || ""
+                if (storedMisEstados == "") {
+                    storedMisEstados = await getXpics(5)
+                    localStorage.setItem("mystories", JSON.stringify(storedMisEstados))
+                    setMisEstados(storedMisEstados)
+                } else {
+                    setMisEstados(storedMisEstados)
+                }
+
+
+
+                setMiPerfil({
+                    ...storedPerfil,
+                    "posts": [...storedMyPosts],
+                    "estadosGuardados": [...storedMisEstados]
+                })
+                setIsLoaded(true)
+
+            }
+
+        }
+
+        loadData()
+
+    }, [])
+
+    return (
         <section>
-            <InfoPerfil/>
+            {isLoaded && <InfoPerfil perfil={miPerfil}/>}
+            {isLoaded && <PublicacionesPerfil perfil={miPerfil}/>}
         </section>
     )
 }
